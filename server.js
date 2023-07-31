91751a53bc6b7ac51777668fa5c619675c467eb7
Surya-7890 <suryamariappan9108@gmail.com>
2023-07-31 15:47:31 +0530

    added server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose.connect('mongodb://127.0.0.1:27017/anonymous')
+mongoose.connection.once('open', () => console.log('connected to db'));
+
+app.use(express.json())
+app.use(cors({
+  origin: '*'
+}));
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(7000, () => console.log('server running'));
