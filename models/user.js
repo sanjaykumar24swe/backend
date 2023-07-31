36f2af26ff22ca433cc917afeb78484a0cfcd285
Surya-7890 <suryamariappan9108@gmail.com>
2023-07-31 15:04:03 +0530

    added mongoose models

---
+++ models/user.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Username Is Required']
+  },
+  questions: {
+    type: Array,
+    default: []
+  }
+});
+
+module.exports = mongoose.model('User', UserSchema);
