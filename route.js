56ff5ee1f849ae7a394ebe04a35acddc07fd5b03
Sanjaykumar <sanjaykumar24official@gmail.com>
2023-07-31 16:04:44 +0530

    updated server.js

--- route.js
+++ route.js
@@ -6,20 +6,36 @@ route.post("/question",async(req,res)=>{
    const newUser = await user.findOne({name});
    newUser.questions.push(question);
    await newUser.save();
+   res.send({sucess:true})
   }
   catch(e)
   {
-    console.log(e)
+    console.log(e.message)
+    res.send(e.message)
   }
 })
 route.post("/:name",async(req,res)=>{
     const {name} = req.body;
     try{
-    user.create({
+    await user.create({
         name
     })
+    res.send({sucess:true});
 }catch(e){
 console.log(e.message);
+res.send(e.message)
 }
  })
- module.export = router;
+ route.get("/fetch",async(req,res)=>{
+    const {name} = req.body;
+    try{
+        const newUser = await user.findOne({name});
+        res.send(newUser);
+    }
+    catch(e)
+    {
+        console.log(e.message)
+        res.send(e.message)
+    }
+ })
+ module.export = route;
--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
+const route = require('./route.js')
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
-
 mongoose.connect('mongodb://127.0.0.1:27017/anonymous')
 mongoose.connection.once('open', () => console.log('connected to db'));
-
 app.use(express.json())
 app.use(cors({
   origin: '*'
 }));
 app.use(express.urlencoded({ extended: false }));
-
+app.use("/",route)
 app.listen(7000, () => console.log('server running'));
